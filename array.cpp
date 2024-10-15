b67c80f89effe1881d120e1385828bf767ca8d41
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-10-15 20:24:03 +0530

    added array code

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[5];
+    a[0]=1;
+    a[1]=2;
+    a[2]=3;
+    a[3]=4;
+    for(int i=0;i<4;i++){
+        cout<<a[i]<<endl;
+    }
+}
